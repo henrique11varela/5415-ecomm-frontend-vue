08c547f43510524edf6cd70764bf557d09f83cc8
PedroNito <Pedro.nito.23@gmail.com>
2023-12-05 21:09:38 +0000

    product created

---
+++ src/components/Product.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="">
+      <h2>{{ product.name }}</h2>
+      <p>{{ product.description }}</p>
+      <p>Price: € {{ product.price }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      product: {
+        type: Object,
+        required: true
+      }
+    }
+  };
+  </script>
