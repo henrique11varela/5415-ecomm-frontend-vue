4e379f38f4b09e3d11abbb4142faff676a3a9a84
Goncalo Moreira <goncalo.moreira.t0123177@edu.atec.pt>
2023-12-21 22:39:49 +0000

    End Contact
    Contact ended , needs to have more style? (:)

--- src/components/Contact/ContactMap.vue
+++ src/components/Contact/ContactMap.vue
@@ -1,9 +1,9 @@
 <template>
     <!-- MAPA -->
-            <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001.063313994167!2d-8.688849824244368!3d41.22039070633754!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd2468ec3655d101%3A0x12cab54b20119109!2sATEC%20-%20Academia%20de%20Forma%C3%A7%C3%A3o%20(Matosinhos)!5e0!3m2!1sen!2spt!4v1701810264143!5m2!1sen!2spt"
-                class="h-full" style="border:0;" allowfullscreen="" loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <iframe
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12004.25393626439!2d-8.686275!3d41.220387!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd2468ec3655d101%3A0x12cab54b20119109!2sATEC%20-%20Academia%20de%20Forma%C3%A7%C3%A3o%20(Matosinhos)!5e0!3m2!1sen!2spt!4v1703197303821!5m2!1sen!2spt"
+        width=100% height="498" style="border:0;" allowfullscreen="" loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"></iframe>
 </template>
 
 <script>
