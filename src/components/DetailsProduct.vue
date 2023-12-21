7524473a18c42fd5b47eb50825d9076a68191efd
Fábio <fabiomiguel3.10@gmail.com>
2023-12-21 23:29:39 +0000

    Manual input on quantity working
    still need to add logic to add to cart

--- src/components/DetailsProduct.vue
+++ src/components/DetailsProduct.vue
@@ -11,6 +11,8 @@ const props = defineProps({
 
 let buyQuantity = ref(1);
 
+const inputQuantity = ref(null);
+
 onMounted(() => {
   console.log(props.product)
 })
@@ -25,6 +27,12 @@ function increment() {
 
 function decrement() {
   if(buyQuantity.value > 1) buyQuantity.value--;
+  console.log(buyQuantity)
+}
+
+function inputVerifications() {
+   if(buyQuantity.value > props.product.quantity) buyQuantity.value = props.product.quantity;
+   if(buyQuantity.value < 1) buyQuantity.value = 1;
   console.log(buyQuantity)
 }
 
@@ -52,7 +60,7 @@ function decrement() {
                   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h16"/>
                 </svg>
               </button>
-              <input type="text" id="quantity-input" aria-describedby="helper-text-explanation" class="bg-gray-50 border-l-0 border-r-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" :placeholder="buyQuantity" required>
+              <input type="number" v-model="buyQuantity" @input="inputVerifications" id="quantity-input" aria-describedby="helper-text-explanation" class="bg-gray-50 border-l-0 border-r-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
               <button type="button" ref="increment-button" @click="increment" id="increment-button" data-action="increment" class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-r-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
                 <svg class="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
