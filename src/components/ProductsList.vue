a55fa8eff44100787addf060da42852a91373a14
Fábio <fabiomiguel3.10@gmail.com>
2023-12-22 22:26:08 +0000

    added icon to navbar and set fixed width on products

--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <div class="flex flex-col mx-8 gap-8 md:flex-wrap md:max-w-full md:flex-1 md:flex-row md:my-10 md:justify-center md:px-40 md:mx-20">
+  <div class="flex justify-center">
+    <div class="flex flex-col mx-8 my-4 gap-8 md:flex-wrap md:max-w-full md:flex-1 md:flex-row md:my-10 justify-center md:px-40 md:mx-20">
 
-      <CardShell class="w-full h-50 md:max-w-48" v-for="product in productList" :key="product.id" tiltable shiny>
+      <CardShell class="w-80 h-50 md:max-w-48" v-for="product in productList" :key="product.id" tiltable shiny>
         <Product :product="product" />
       </CardShell>
     </div>
