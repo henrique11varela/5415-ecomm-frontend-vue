7fca239276a627ac4ecd38b41d563ca45ce12e00
Henrique Varela <henrique11varela@gmail.com>
2023-12-31 03:55:08 +0000

    1 source of truth for api ip

--- src/services/couponService.js
+++ src/services/couponService.js
@@ -1,25 +1,21 @@
-
+import { API_URL } from "../../env.js";
 
 export async function verifyCoupon(couponCode) {
-    
-    const API_URL = 'http://127.0.0.1:3333'; 
-
-        try {
-            const response = await fetch(API_URL + "/check-coupon", {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ couponCode: couponCode }),
-            });
-            
-            const data = await response.json();
-            
-            return (data.success)
-                ? { response: true, message: data.message, discount: data.discount }
-                : { response: false, message: data.error };
-        } catch (error) {
-            console.log(`Error: ${error}`);
-        }
-
+    try {
+        const response = await fetch(API_URL + "/check-coupon", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ couponCode: couponCode }),
+        });
+        
+        const data = await response.json();
+        
+        return (data.success)
+            ? { response: true, message: data.message, discount: data.discount }
+            : { response: false, message: data.error };
+    } catch (error) {
+        console.log(`Error: ${error}`);
     }
+}
--- src/services/productService.js
+++ src/services/productService.js
@@ -1,19 +1,8 @@
-// import axios from "axios";
-
-// Replace this URL with API endpoint (after npm run dev)
-const API_URL = 'http://127.0.0.1:3333';
+import { API_URL } from "../../env.js";
 
 export function useProductService() {
     async function fetchProducts() {
         try {
-            // const response = await axios.get(API_URL);
-
-            // if (response.status !== 200) {
-            //     throw new Error(`Failed to fetch products: ${response.statusText}`);
-            // }
-
-            // const products = response.data;
-            // return products;
             const response = await fetch(`${API_URL}/products`);
             const data = await response.json();
             return data;
@@ -22,6 +11,5 @@ export function useProductService() {
             throw error;
         }
     }
-
     return { fetchProducts };
 }
